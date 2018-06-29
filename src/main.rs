ee2479605ceb9e8c0e38309d41b42297dfa230c0
Adrian Stanciu <astanciu@Adrians-MacBook-Pro.local>
2018-06-29 19:49:23 +0300

    CHIP-8: change screen to be the only one handling sdl2 stuff

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ use std::io::Read;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+
+use sdl2::render::{Canvas, Texture, TextureCreator};
+use sdl2::rect::{Point, Rect};
+
 use std::collections::HashSet;
 use std::time::Duration;
 
@@ -17,7 +21,7 @@ pub mod interpreter;
 pub mod keyboard;
 pub mod screen;
 
-// use disassembler::disassembler::Disassembler;
+use disassembler::disassembler::Disassembler;
 use interpreter::interpreter::Interpreter;
 use keyboard::keyboard::Keyboard;
 use screen::screen::Screen;
@@ -27,29 +31,34 @@ pub fn main() -> std::io::Result<()> {
     let mut file = File::open("TETRIS")?;
     let mut program = Vec::with_capacity(size as usize);
     let _bytes_read = file.read_to_end(&mut program)?;
-    // let disassembly = Disassembler::from_binary(&program);
+    {
+        let disassembly = Disassembler::from_binary(&program);
+    }
     let mut display = [0u8; 64 * 32];
     let mut screen = Screen::with_display(&mut display);
     let sdl_context = sdl2::init().unwrap();
+    let mut keyboard = Keyboard::with_context(&sdl_context);
     let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("rust-sdl2 demo: Video", 640, 320)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.clear();
+    canvas.present();
     {
-        let mut keyboard = Keyboard::with_context(&sdl_context);
         let mut interpreter = Interpreter::new(&mut keyboard, &mut program, &mut screen);
-        let window = video_subsystem
-            .window("rust-sdl2 demo: Video", 800, 600)
-            .position_centered()
-            .opengl()
-            .build()
-            .unwrap();
-
-        let mut canvas = window.into_canvas().build().unwrap();
-
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
-        canvas.present();
         interpreter.run();
     }
-
+    let texture_creator: TextureCreator<_> = canvas.texture_creator();
+    let mut square_texture1: Texture = texture_creator.create_texture_target(None, 10, 10).unwrap();
+    canvas.copy(&square_texture1, None, Rect::new(0, 0, 10, 10)).unwrap();
+    canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
         for event in event_pump.poll_iter() {
--- src/screen.rs
+++ src/screen.rs
@@ -1,35 +1,38 @@
+use sdl2::pixels;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+
 pub mod screen {
-    #[derive(Debug)]
-    pub struct Screen<'a> {
-        screen: &'a mut [u8],
+    pub struct Screen {
+        canvas: Canvas<Window>,
     }
 
-    impl<'a> Screen<'a> {
-        pub fn with_display(display: &mut [u8]) -> Screen {
-            Screen { screen: display }
+    pub const SCALE: u8 = 20;
+    pub const SCREEN_WIDTH: u32 = (CHIP8_WIDTH as u32) * SCALE;
+    pub const SCREEN_HEIGHT: u32 = (CHIP8_HEIGHT as u32) * SCALE;
+
+    impl Screen {
+        pub fn new(sdl_context: &sdl2::Sdl) -> Self {
+            let video_sys = sdl_context.video().unwrap();
+            let window = video_sys
+                .window("CHIP-8", SCREEN_WIDTH, SCREEN_HEIGHT)
+                .position_centered()
+                .opengl()
+                .build()
+                .unwrap();
+
+            let mut canvas = window.into_canvas().build.unwrap();
+
+            canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
+            canvas.clear();
+            canvas.present();
+
+            Screen { canvas: canvas }
         }
 
-        pub fn render(&self) {
-            println!("Rendering the screen");
-        }
-
-        #[inline(always)]
-        pub fn size(&self) -> usize {
-            self.screen.len()
-        }
-
-        #[inline(always)]
-        pub fn width(&self) -> usize {
-            32
-        }
-
-        #[inline(always)]
-        pub fn height(&self) -> usize {
-            64
-        }
-        #[inline(always)]
-        pub fn screen(&mut self) -> &mut [u8] {
-            self.screen
+        pub fn draw(&mut self, pixels: &[u8]) {
+            unimplemented!();
         }
     }
 
